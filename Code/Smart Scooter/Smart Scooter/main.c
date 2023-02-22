3200aecd46dc79f3a156fd72f3b23773fc7c3a8f
Ahmed <ahmedrsamak@gmail.com>
2023-02-22 21:56:59 +0200

    First Commit

---
+++ Code/Smart Scooter/Smart Scooter/main.c
@@ -0,0 +1,18 @@
+/*
+ * Smart Scooter.c
+ *
+ * Created: 2/22/2023 9:50:25 PM
+ * Author : ahmed
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
