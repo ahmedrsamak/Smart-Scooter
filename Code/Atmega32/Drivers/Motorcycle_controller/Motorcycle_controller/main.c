7fafa6b341f327400f36b4597250a03aed9add1a
Ahmed <ahmedrsamak@gmail.com>
2023-03-27 19:03:11 +0200

    added Mortorcycle_controller and removed unused drivers

---
+++ Code/Atmega32/Drivers/Motorcycle_controller/Motorcycle_controller/main.c
@@ -0,0 +1,18 @@
+/*
+ * Motorcycle_controller.c
+ *
+ * Created: 3/27/2023 7:01:56 PM
+ * Author : ahmed
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
